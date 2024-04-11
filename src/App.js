8fdc856da5dcdc6eab345d6f711c451d076f6795
Brent Gauthier <cajundrummer@gmail.com>
2024-04-10 23:19:48 -0400

    Add component to main page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Accordion from './Components/Accordion'
 import RandomColorGenerator from './Components/RandomColorGenerator'
 import StarRating from './Components/StarRating'
+import ImageSlider from './Components/ImageSlider'
 
 const App = () => {
   return (
@@ -15,6 +16,13 @@ const App = () => {
       <hr className="linebreak" />
       <h1>Star Rating Component</h1>
       <StarRating numOfStars={10} />
+      <hr className="linebreak" />
+      <h1>Image Slider Component</h1>
+      <ImageSlider 
+        url = {'https://picsum.photos/v2/list'} 
+        page = {'1'} 
+        limit = {'10'} 
+      />
     </div>
   )
 }
